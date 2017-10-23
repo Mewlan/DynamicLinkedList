49d93b4fe41b4ff5f9e4177f94e63eb089885d60
Mewlan <iliq@me.com>
2017-10-24 00:38:38 +0800

    Segmentaion Fault fixed, Impilicite Decleration fault Fixed

--- DynamicLinkedListModelViewController.c
+++ DynamicLinkedListModelViewController.c
@@ -1,5 +1,5 @@
 //
-//  DynamicLinkedListModelViewController.c
+//  DynamicLinkedListModelModelViewController.c
 //  DynamicLinkedList
 //
 //  Created by Mewlan Musajan on 10/23/17.
@@ -8,25 +8,19 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
 #define LEN sizeof(struct Student)
 
-void model();
-void view(struct Student *head, struct Student *ptr1, struct Student *ptr2);
-void outPutViewController(struct Student *head);
-struct Student * dynamicLinkedListAllocationController(struct Student *ptr1, struct Student *ptr2, struct Student *head);
-
+int N;
 struct Student
 {
-	long num;
+	long int num;
 	float score;
 	struct Student *next;
 };
 
-int n;
-
-int main(int argc, char const *argv[])
+int main(int argc, char const *argv[])					
 {
+	void model();
 	model();
 	return 0;
 }
@@ -34,38 +28,36 @@ int main(int argc, char const *argv[])
 void model()
 {
 	struct Student *head, *ptr1, *ptr2;
-	n = 0;
-	ptr1 = ptr2 = (struct Student *) malloc(LEN);
-	head = NULL;
+	ptr1 = ptr2 = (struct Student*) malloc(LEN);
+	void view(struct Student *head, struct Student *ptr1, struct Student *ptr2);
 	view(head, ptr1, ptr2);
 }
 
 void view(struct Student *head, struct Student *ptr1, struct Student *ptr2)
 {
-	outPutViewController(dynamicLinkedListAllocationController(ptr1, ptr2, head));
+	void outPutViewController(struct Student *head);
+	struct Student * dynamicLinkedListAllocationController(struct Student *head, struct Student *ptr1, struct Student *ptr2);
+	outPutViewController(dynamicLinkedListAllocationController(head, ptr1, ptr2));
 }
 
 void outPutViewController(struct Student *head)
 {
 	struct Student *ptr;
-	printf("\nNow, These %d records are:\n", n);
 	ptr = head;
-	if (head != NULL)
-	{
-		do {
-			printf("%ld %5.1f\n", ptr->num, ptr->score);
-			ptr = ptr->next;
-		} while(ptr != NULL);
+	while (ptr != NULL) {
+		printf("%ld, %f\n", ptr->num, ptr->score);
+		ptr = ptr->next;
 	}
 }
 
-struct Student * dynamicLinkedListAllocationController(struct Student *ptr1, struct Student *ptr2, struct Student *head)
+struct Student * dynamicLinkedListAllocationController(struct Student *head, struct Student *ptr1, struct Student *ptr2)
 {
 	scanf("%ld, %f", &ptr1->num, &ptr1->score);
+	N = 0;
 	head = NULL;
-	while(ptr1->num != 0) {
-		++n;
-		if (n == 1)
+	while (ptr1->num != 0) {
+		++N;
+		if (N == 1)
 		{
 			head = ptr1;
 		} else {
@@ -74,10 +66,13 @@ struct Student * dynamicLinkedListAllocationController(struct Student *ptr1, str
 		ptr2 = ptr1;
 		ptr1 = (struct Student*) malloc(LEN);
 		scanf("%ld, %f", &ptr1->num, &ptr1->score);
-	}
+	} 
 	ptr2->next = NULL;
 	return(head);
 }
+
+
+
 
 
 
